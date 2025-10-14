195605eed644d8b9dc83702abdd8c003427afaeb
Adso <Ca_0051@hotmail.com>
2025-10-14 18:00:06 -0500

    first commit

---
+++ database/migrations/8_2025_10_14_202454_create_chatssupport_table.php
@@ -0,0 +1,25 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('chatssupport', function (Blueprint $table) {
+            $table->id();
+            $table->text('mensaje');
+            $table->boolean('attended');
+            $table->timestamp('date_message'); 
+            
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('chatssupport');
+    }
+};
